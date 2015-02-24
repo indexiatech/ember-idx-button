ece147f1ed99fd69ad0421ecc4272d549c144241
Asaf Shakarzy <asaf@indexia.co>
2015-02-24 12:51:49 +0200

    0.1.3 release
    - Upgraded to latest idx-utils
    - Docs for changing state of a button externally.

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -2,6 +2,7 @@ import Em from "ember";
 
 export default Em.Controller.extend({
   message: void 0,
+  someState: 'default',
   actions: {
     onClick: function() {
       return this.set('message', 'Clicked!');
@@ -19,6 +20,15 @@ export default Em.Controller.extend({
           return rej("Server error...");
         }, 1500);
       }));
+    },
+    someButton: function() {
+      var promise = new Promise(function(res, rej) {
+        return Em.run.later(function() {
+          return res("YES");
+        }, 1500);
+      });
+      this.set('someState', 'executing');
+      this.set('somePromise', promise);
     }
   }
 });
